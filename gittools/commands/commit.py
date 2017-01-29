065f0f55825e03e8725926157bc0ba3f4191d95a
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2017-01-29 03:17:17 +0100

    Added commit command.

---
+++ gittools/commands/commit.py
@@ -0,0 +1,37 @@
+
+from scriptcore.console.cuiscript import CuiScript
+
+
+class Commit(CuiScript):
+
+    def __init__(self, base_path, arguments=None):
+        """
+        Construct the script
+        :param base_path:   The base path
+        :param arguments:   The arguments
+        """
+
+        title = 'Git commit'
+        description = 'Record changes to the repository'
+
+        super(Commit, self).__init__(base_path, title, description, arguments=arguments)
+
+        self._register_option('m', 'Commit message')
+
+    def run(self):
+        """
+        Run the script
+        """
+
+        self._analyze_arguments()
+
+        if self._has_option('m'):
+            message = self._get_option('m')
+        else:
+            message = self.input('Specify your commit message:')
+
+        if not message:
+            self.output('No message specified', 'error')
+        else:
+            self.execute('git commit -m "%s"' % message)
+            self.execute('git rev-parse HEAD | pbcopy')
